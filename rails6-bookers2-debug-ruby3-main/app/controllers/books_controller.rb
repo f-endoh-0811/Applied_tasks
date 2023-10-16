2ca02cc1b6183aac20784760df5f2dc8ad45b5f3
aahfugetnsosji <126599436+aahfugetnsosji@users.noreply.github.com>
2023-10-16 13:07:24 +0000

    [Update] 投稿者以外の投稿編集・削除制限

--- rails6-bookers2-debug-ruby3-main/app/controllers/books_controller.rb
+++ rails6-bookers2-debug-ruby3-main/app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  before_action :book_find, only: [:edit, :update, :destroy]
 
   def show
     @book_find = Book.find(params[:id])
@@ -22,12 +23,14 @@ class BooksController < ApplicationController
   end
 
   def edit
-    @book = Book.find(params[:id])
+    if @book.user_id != current_user.id
+      redirect_to book_path(@book)
+    end
   end
 
   def update
-    @book = Book.find(params[:id])
-    if @book.update(book_params)
+    if @book.user_id == current_user.id
+      @book.update(book_params)
       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
       render "edit"
@@ -35,14 +38,21 @@ class BooksController < ApplicationController
   end
 
   def destroy
-    @book = Book.find(params[:id])
-    @book.destroy
-    redirect_to books_path
+    if @book.user_id == current_user.id
+      @book.destroy
+      redirect_to books_path
+    else
+      render "show"
+    end
   end
 
   private
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def book_find
+    @book = Book.find(params[:id])
   end
 end

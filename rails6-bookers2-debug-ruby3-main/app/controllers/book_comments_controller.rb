22d1753b174ab74685a66851904685852b329ab6
aahfugetnsosji <126599436+aahfugetnsosji@users.noreply.github.com>
2023-11-17 06:55:33 +0000

    [Update] コメント機能

--- rails6-bookers2-debug-ruby3-main/app/controllers/book_comments_controller.rb
+++ rails6-bookers2-debug-ruby3-main/app/controllers/book_comments_controller.rb
@@ -1,17 +1,17 @@
 class BookCommentsController < ApplicationController
 
   def create
-    # book = Book.find(params[:book_id])
-    # comment = current_user.book_comments.new(book_comment_params)
-    # comment.book_id = book.id
-    # if comment.save
-    #   redirect_to book_path(book)
-    # else
-    #   @book_find = Book.find(params[:id])
-    #   @book = Book.new
-    #   @comment = BookComment.new
-    #   render template: "books/show"
-    # end
+    book = Book.find(params[:book_id])
+    comment = current_user.book_comments.new(book_comment_params)
+    comment.book_id = book.id
+    if comment.save
+      redirect_to book_path(book)
+    else
+      @book_find = Book.find(params[:id])
+      @book = Book.new
+      @comment = BookComment.new
+      render template: "books/show"
+    end
   end
 
   def destroy

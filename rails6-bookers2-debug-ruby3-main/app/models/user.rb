4f303703df43b8d494920643a44e79e6214ac008
aahfugetnsosji <126599436+aahfugetnsosji@users.noreply.github.com>
2023-12-14 14:05:08 +0000

    [Finish] フォロー機能

--- rails6-bookers2-debug-ruby3-main/app/models/user.rb
+++ rails6-bookers2-debug-ruby3-main/app/models/user.rb
@@ -7,20 +7,34 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-  # 能動的関係(フォローする側)
-  has_many :active_relationships, class_name: "Relationship",
-                                  foreign_key: "follower_id",
-                                  dependent: :destroy
-  # 受動的関係(フォローされる側)
-  
+  # 能動的関係(あるユーザーをフォローしているすべての集合)の関連付け
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # 受動的関係(あるユーザーにフォローされているすべての集合)の関連付け
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  # フォローしているユーザーを取得
+  has_many :followings, through: :active_relationships, source: :followed
+  # フォロワーを取得
+  has_many :followers, through: :passive_relationships, source: :follower
+
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: {maximum: 200 }
 
-
-
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
+
+  # ユーザーをフォローする
+  def follow(user)
+    active_relationships.create(followed_id: user.id)
+  end
+  # ユーザーをアンフォローする
+  def unfollow(user)
+    active_relationships.find_by(followed_id: user.id).destroy
+  end
+  # 指定したユーザーをフォローしているかどうか判定(していればtrueを返す)
+  def following?(user)
+    followings.include?(user)
   end
 end
